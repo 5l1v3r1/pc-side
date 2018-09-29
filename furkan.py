52ebdcb92566806e56b849153dd3a1ff009a97df
Unknown <furkanarslan3469@gmail.com>
2018-09-29 17:46:03 +0300

    Add latest sound codes

--- furkan.py
+++ furkan.py
@@ -13,21 +13,21 @@ sdFlagOnce = 0
 
 while True:
     if ((sd.getBoolean("init", False) == True) and sdFlagForInit == 0):
-        playsound('C:/ Users/mars/Desktop/sesler/robotu-aciyorum.mp3')
+        playsound('C:/Users/mars/Desktop/sesler/robotu-aciyorum.mp3')
         sdFlagForInit = 1
     
     if ((sd.getBoolean("Limit Switch Status", False) == True) and sdFlagForBox == 0):
-        playsound('C:/ Users/mars/Desktop/sesler/kutuyu-aldim.mp3')
+        playsound('C:/Users/mars/Desktop/sesler/kutuyu-aldim.mp3')
         sdFlagForBox = 1
         
     if ((sd.getBoolean("releaseCube", False) == True) and sdFlagForBox == 1):
-        playsound('C:/ Users/mars/Desktop/sesler/kutuyu-atiyorum.mp3')
+        playsound('C:/Users/mars/Desktop/sesler/kutuyu-atiyorum.mp3')
         sdFlagForBox = 0
     
-    if ((sd.getDouble("tyme", 0.0) == 50.0) and sdFlagOnce == 0):
-        playsound('C:/ Users/mars/Desktop/sesler/son-50.mp3')
+    if ((sd.getNumber("time", 0.0) == 50.0) and sdFlagOnce == 0):
+        playsound('C:/Users/mars/Desktop/sesler/son-50.mp3')
         sdFlagOnce = 1
     
-    if ((sd.getDouble("tyme", 0.0) == 50.0) and sdFlagOnce == 0):
-        playsound('C:/ Users/mars/Desktop/sesler/do-it.mp3')
+    if ((sd.getNumber("tyme", 0.0) == 35.0) and sdFlagOnce == 0):
+        playsound('C:/Users/mars/Desktop/sesler/do-it.mp3')
         sdFlagOnce1 = 1
