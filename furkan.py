e519fb60da8249b6f93ba839df538d37d0f7c7e9
furkanarslan7108 <41804017+furkanarslan7108@users.noreply.github.com>
2018-10-04 16:40:47 +0300

    Add latest code. Python is cancer.

--- furkan.py
+++ furkan.py
@@ -1,7 +1,5 @@
 from playsound import playsound 
-from networktables import  NetworkTables
-# import logging
-# logging.basicConfig(level=logging.DEBUG)
+from networktables import NetworkTables
 
 ip = "10.71.8.2"
 
@@ -10,24 +8,49 @@ sd = NetworkTables.getTable("datatable")
 sdFlagForInit = 0
 sdFlagForBox = 0
 sdFlagOnce = 0
-
+sdFlagOnce1 = 0
+    
 while True:
-    if ((sd.getBoolean("init", False) == True) and sdFlagForInit == 0):
-        playsound('C:/Users/mars/Desktop/sesler/robotu-aciyorum.mp3')
-        sdFlagForInit = 1
+
+    #Türkçe        
+        if ((sd.getBoolean("init", False) == True) and sdFlagForInit == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Tur/robotu-aciyorum.mp3')
+            sdFlagForInit = 1
     
-    if ((sd.getBoolean("Limit Switch Status", False) == True) and sdFlagForBox == 0):
-        playsound('C:/Users/mars/Desktop/sesler/kutuyu-aldim.mp3')
-        sdFlagForBox = 1
+        if ((sd.getBoolean("Limit Switch Status", False) == True) and sdFlagForBox == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Tur/kutuyu-aldim.mp3')
+            sdFlagForBox = 1
         
-    if ((sd.getBoolean("releaseCube", False) == True) and sdFlagForBox == 1):
-        playsound('C:/Users/mars/Desktop/sesler/kutuyu-atiyorum.mp3')
-        sdFlagForBox = 0
+        if ((sd.getBoolean("releaseCube", False) == True) and sdFlagForBox == 1):
+            playsound('C:/Users/mars/Desktop/sesler/Tur/kutuyu-atiyorum.mp3')
+            sdFlagForBox = 0
     
-    if ((sd.getNumber("time", 0.0) == 50.0) and sdFlagOnce == 0):
-        playsound('C:/Users/mars/Desktop/sesler/son-50.mp3')
-        sdFlagOnce = 1
-    
-    if ((sd.getNumber("tyme", 0.0) == 35.0) and sdFlagOnce == 0):
-        playsound('C:/Users/mars/Desktop/sesler/do-it.mp3')
-        sdFlagOnce1 = 1
+        if ((sd.getNumber("time", 0.0) == 50.0) and sdFlagOnce == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Tur/son-50.mp3')
+            sdFlagOnce = 1
+            
+        if ((sd.getNumber("tyme", 0.0) == 35.0) and sdFlagOnce1 == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Tur/do-it.mp3')
+            sdFlagOnce1 = 1
+            
+    #English
+        if ((sd.getBoolean("init", False) == True) and sdFlagForInit == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Eng')
+            sdFlagForInit = 1
+      
+        if ((sd.getBoolean("Limit Switch Status", False) == True) and sdFlagForBox == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Eng')
+            sdFlagForBox = 1
+        
+        if ((sd.getBoolean("releaseCube", False) == True) and sdFlagForBox == 1):
+            playsound('C:/Users/mars/Desktop/sesler/Eng')
+            sdFlagForBox = 0
+      
+        if ((sd.getNumber("time", 0.0) == 50.0) and sdFlagOnce == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Eng')
+            sdFlagOnce = 1
+        
+        if ((sd.getNumber("tyme", 0.0) == 35.0) and sdFlagOnce1 == 0):
+            playsound('C:/Users/mars/Desktop/sesler/Eng')
+            sdFlagOnce1 = 1
+      
